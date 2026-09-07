565fc39b46800e535258c22793e94e99bb9573df
Wkcf <3072296612@qq.com>
2026-09-07 14:43:32 +0800

    chore: 初始化工程（HarmonyOS + ArkUI-X 模板，com.example.myapplication）

---
+++ entry/hvigorfile.ts
@@ -0,0 +1,7 @@
+// Script for compiling build behavior. It is built in the build plug-in and cannot be modified currently.
+import { HapTasks } from '@ohos/hvigor-ohos-arkui-x-plugin';
+
+export default {
+    system: HapTasks, /* Built-in plugin of Hvigor. It cannot be modified. */
+    plugins: []       /* Custom plugin to extend the functionality of Hvigor. */
+}
---
+++ hvigorfile.ts
@@ -0,0 +1,7 @@
+// Script for compiling build behavior. It is built in the build plug-in and cannot be modified currently.
+import { AppTasksForArkUIX } from '@ohos/hvigor-ohos-arkui-x-plugin';
+
+export default {
+  system: AppTasksForArkUIX, /* Built-in plugin of Hvigor. It cannot be modified. */
+  plugins: []       /* Custom plugin to extend the functionality of Hvigor. */
+}
