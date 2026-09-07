ccc6a1141f1359137dfd08f84d2ba88748f921e1
Wkcf <3072296612@qq.com>
2026-09-07 16:08:43 +0800

    feat: 语音计算器接入语音识别/合成，工程移除 ArkUI-X 转纯鸿蒙

--- entry/hvigorfile.ts
+++ entry/hvigorfile.ts
@@ -1,7 +1,7 @@
 // Script for compiling build behavior. It is built in the build plug-in and cannot be modified currently.
-import { HapTasks } from '@ohos/hvigor-ohos-arkui-x-plugin';
+import { hapTasks } from '@ohos/hvigor-ohos-plugin';
 
 export default {
-    system: HapTasks, /* Built-in plugin of Hvigor. It cannot be modified. */
-    plugins: []       /* Custom plugin to extend the functionality of Hvigor. */
+  system: hapTasks, /* Built-in plugin of Hvigor. It cannot be modified. */
+  plugins: []       /* Custom plugin to extend the functionality of Hvigor. */
 }
--- hvigorfile.ts
+++ hvigorfile.ts
@@ -1,7 +1,7 @@
 // Script for compiling build behavior. It is built in the build plug-in and cannot be modified currently.
-import { AppTasksForArkUIX } from '@ohos/hvigor-ohos-arkui-x-plugin';
+import { appTasks } from '@ohos/hvigor-ohos-plugin';
 
 export default {
-  system: AppTasksForArkUIX, /* Built-in plugin of Hvigor. It cannot be modified. */
+  system: appTasks, /* Built-in plugin of Hvigor. It cannot be modified. */
   plugins: []       /* Custom plugin to extend the functionality of Hvigor. */
 }
